b0fc50ef8d6d5a9e4382ee79f093e9ed6a0342d2
Gal Shilo <gal.shilo@PRGA-006418.local>
2019-03-21 13:12:52 +0100

    fixed some issues

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ function ChromeExtensionManifest(options) {
   this.options = options || {};
   this.context = path.dirname(module.parent.filename);
   this.options.inputFile = path.isAbsolute(this.options.inputFile) ? this.options.inputFile : join(this.context, this.options.inputFile);
-  if (!fs.existsSync(this.options.inputFile)){
-    throw 'Input file does not exists';
-  }
   this.outputFile = path.isAbsolute(this.options.outputFile) ? this.options.outputFile : join(this.context, this.options.outputFile);
-  this.props = this.options.props instanceof Object ? this.options.props : throw 'props must be an object';
+  this.props = this.options.props instanceof Object ? this.options.props : {};
 }
 
 // hook into webpack
-Plugin.prototype.apply = function(compiler) {
+ChromeExtensionManifest.prototype.apply = function(compiler) {
   var self = this;
   return compiler.plugin('done', function() {
     self.createManifst.call(self);
@@ -22,17 +19,14 @@ Plugin.prototype.apply = function(compiler) {
 }
 
 // package the extension
-Plugin.prototype.createManifst = function() {
+ChromeExtensionManifest.prototype.createManifst = function() {
   var self = this;
   var manifestContent = require(this.options.inputFile);
-  if (!manifestContent){
-    throw 'Corrupted manifest content'
-  }
   let newManifestContent = {
     ...manifestContent,
     ...this.props
   };
-  fs.writeFile(self.outputFile, newManifestContent);
+  fs.writeFileSync(self.outputFile, JSON.stringify(newManifestContent, null, 4));
 };
 
 module.exports = ChromeExtensionManifest;
