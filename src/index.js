e2a34c4136df18044f6752f60f1a09672286597c
Gal Shilo <gal.shilo@PRGA-006418.local>
2019-03-21 10:43:03 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-const defaultAwesomeFunction = (name) => {
-  const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
-  return returnStr;
+var fs = require('fs');
+var path = require('path');
+var join = path.join;
+
+function Plugin(options) {
+  this.options = options || {};
+  this.context = path.dirname(module.parent.filename);
+  this.options.inputFile = path.isAbsolute(this.options.inputFile) ? this.options.inputFile : join(this.context, this.options.inputFile);
+  if (!fs.existsSync(this.options.inputFile)){
+    throw 'Input file does not exists';
+  }
+  this.outputFile = path.isAbsolute(this.options.outputFile) ? this.options.outputFile : join(this.context, this.options.outputFile);
+  this.props = this.options.props instanceof Object ? this.options.props : throw 'props must be an object';
+}
+
+// hook into webpack
+Plugin.prototype.apply = function(compiler) {
+  var self = this;
+  return compiler.plugin('done', function() {
+    self.package.call(self);
+  });
+}
+
+// package the extension
+Plugin.prototype.createManifst = function() {
+  var self = this;
+  var manifestContent = require(this.options.inputFile);
+  if (!manifestContent){
+    throw 'Corrupted manifest content'
+  }
+  let newManifestContent = {
+    ...manifestContent,
+    ...this.props
+  };
+  fs.writeFile(self.outputFile, newManifestContent);
 };
 
-const awesomeFunction = () => 'I am just an Awesome Function';
-
-export default defaultAwesomeFunction;
-
-export { awesomeFunction };
+module.exports = Plugin;
